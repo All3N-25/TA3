4c97bda168b0feacae958ee5576c104d36dd574f
Allen <all3ncalayan25@gmail.com>
2026-02-23 14:30:25 +0800

    first commit

---
+++ Technical3.py
@@ -0,0 +1,14 @@
+productionRules = {
+    'A' : ['da', 'BC'],
+    'B' : ['g', 'e'],
+    'C' : ['h', 'e']
+}
+
+Production = []
+Production.append(productionRules.keys())
+
+
+def First(symbol):
+	
+def Follow(symbol):
+    
