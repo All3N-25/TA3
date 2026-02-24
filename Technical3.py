69804b659a497b448cfd38f94f94764753a2670a
Allen <all3ncalayan25@gmail.com>
2026-02-25 00:28:51 +0800

    Gumagana na first pero hanggang 2 Generators palang

--- Technical3.py
+++ Technical3.py
@@ -1,14 +1,48 @@
+#AI LINK = https://gemini.google.com/share/2e1e5330df8b
+
 productionRules = {
-    'A' : ['da', 'BC'],
-    'B' : ['g', 'e'],
-    'C' : ['h', 'e']
+    'A' : ["da", "BCB"],
+    'B' : ["g", "e"],
+    'C' : ["h", "e"]
 }
 
-Production = []
-Production.append(productionRules.keys())
+Productions = []
+Productions.append(list(productionRules.keys()))   #Values = [A, B, C]
 
-
-def First(symbol):
+#get the non terminal
+#Hangang 2 na generator palang kaya.
+def First(symbol: str) -> list:
+    first = []                  #Empty List
 	
-def Follow(symbol):
+    if symbol == "e":           #Base Case: Epsilon
+        return ["e"]
+
+    #Loop sa productions
+    for RHS in productionRules[symbol]:
+        i = 0
+        value = RHS[i]
+
+        #Generatorlist(dict.fromkeys(first))
+        if value.isupper():
+            gen = First(value)
+            first.extend(gen)
+
+            if "e" in gen:
+                first.pop()     #Alsin ung e
+                value = RHS[i + 1]
+                gen2 = First(value)
+                first.extend(gen2)
+
+        #Terminal
+        else:
+            first.append(value)
+
+    return first
+
     
+#find the non terminal after the given set
+#def Follow(symbol):
+    
+for generator in Productions:
+    first = First("A")
+    print(first)
